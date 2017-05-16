47d6d42688e63695ff054724da3082b17c7bda67
« Julien <j.noyer@digitalworkshop.fr>
2017-05-16 13:01:00 +0200

    Ajout de la route recipe/:id

--- public/js/recipe-list.js
+++ public/js/recipe-list.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
     */
         $('.recipeListItem').click( (evt) => {
             evt.preventDefault()
-            let objectId = $(evt.currentTarget).attr('data-id')
             
+            // Récupération de l'ID du document
+            let objectId = $(evt.currentTarget).attr('data-id');
+
+            // Afficher la page d'édition de la recette
             window.location = 'http://localhost:8080/recipe/' + objectId;
         })
         
+         
 });
