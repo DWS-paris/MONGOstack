81f3048b69beebf7af38d01d404129eada99dd9b
« Julien <j.noyer@digitalworkshop.fr>
2017-05-16 12:00:47 +0200

    Initialisation du projet

---
+++ server.js
@@ -0,0 +1,23 @@
+/*
+Configurer le serveur
+*/
+    // Importer les composants
+    let express = require('express');
+    let bodyParser = require('body-parser');
+
+    // Définir le port
+    let port = 8080;
+    
+    // Initier le serveur
+    let app = express();
+
+    // Configurer le moteur de template
+    app.set('view engine', 'ejs')
+
+
+
+
+/*
+Lancer le serveur
+*/
+    app.listen( port, () => console.log('Le serveur est lancé sur le port ' + port) );
